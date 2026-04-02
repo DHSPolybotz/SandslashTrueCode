fd61cff92186b347382413a648dc6cff885c72bd
Discovery Polybotz 9710 <dhs.polybotz@gmail.com>
2026-04-02 17:54:08 -0400

    Enhance RobotContainer for swerve drive functionality
    Refactor RobotContainer to integrate swerve drive and gamepiece handling motors.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,67 +1,239 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
+// Copyright (c) FIRST and other WPILib contributors. 
 
-package frc.robot;
+// Open Source Software; you can modify and/or share it under the terms of 
 
-import frc.robot.Constants.OperatorConstants;
-import frc.robot.commands.Autos;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
+// the WPILib BSD license file in the root directory of this project. 
 
-/**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
- * subsystems, commands, and trigger mappings) should be declared here.
- */
-public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
-  // Intake subsystem instance
+ 
 
-  // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
+package frc.robot; 
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
-  public RobotContainer() {
-    // Configure the trigger bindings
-    configureBindings();
-  }
+ 
 
-  /**
-   * Use this method to define your trigger->command mappings. Triggers can be created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
-   * predicate, or via the named factories in {@link
-   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
-   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
-   * joysticks}.
-   */
-  private void configureBindings() {
-    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    new Trigger(m_exampleSubsystem::exampleCondition)
-        .onTrue(new ExampleCommand(m_exampleSubsystem));
+import static edu.wpi.first.units.Units.*; 
 
-  // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
-  // cancelling on release.
-  m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
-}
+ 
 
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   */
-  public Command getAutonomousCommand() {
-    // An example command will be run in autonomous
-    return Autos.exampleAuto(m_exampleSubsystem);
+import java.util.List; 
 
-// NOTE: removed stray XboxController and incorrect usage. Use the CommandXboxController
-// instance `m_driverController` above for button bindings.
-  }
-}
+ 
+
+import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType; 
+
+import com.ctre.phoenix6.hardware.TalonFX; 
+
+import com.ctre.phoenix6.swerve.SwerveRequest; 
+
+ 
+
+import edu.wpi.first.math.geometry.Pose2d; 
+
+import edu.wpi.first.math.geometry.Rotation2d; 
+
+import edu.wpi.first.wpilibj2.command.Command; 
+
+import edu.wpi.first.wpilibj2.command.Commands; 
+
+import edu.wpi.first.wpilibj2.command.InstantCommand; 
+
+import edu.wpi.first.wpilibj2.command.RunCommand; 
+
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController; 
+
+import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers; 
+
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction; 
+
+import edu.wpi.first.math.trajectory.TrajectoryConfig; 
+
+import edu.wpi.first.math.trajectory.Trajectory; 
+
+import edu.wpi.first.math.trajectory.TrajectoryGenerator; 
+
+ 
+
+import frc.robot.generated.TunerConstants; 
+
+import frc.robot.subsystems.CommandSwerveDrivetrain; 
+
+ 
+
+public class RobotContainer { 
+
+    public static final boolean IS_WHEELS_ENABLED = true; // Set to TRUE to enable wheels. 
+
+    private double MaxSpeed = 1.0 * TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed 
+
+    private double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond); // 3/4 of a rotation per second max angular velocity 
+
+ 
+
+    /* Setting up bindings for necessary control of the swerve drive platform */ 
+
+    private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric() 
+
+            .withDeadband(MaxSpeed * 0.1).withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband 
+
+            .withDriveRequestType(DriveRequestType.OpenLoopVoltage); // Use open-loop control for drive motors 
+
+    private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake(); 
+
+    private final SwerveRequest.PointWheelsAt point = new SwerveRequest.PointWheelsAt(); 
+
+ 
+
+    private final Telemetry logger = new Telemetry(MaxSpeed); 
+
+ 
+
+    private final CommandXboxController joystick = new CommandXboxController(0); 
+
+ 
+
+    public final CommandSwerveDrivetrain drivetrain = TunerConstants.createDrivetrain(); 
+
+ 
+
+    // Old RobotContainer constructor without gamepiece handling motors. 
+
+ 
+
+    // public RobotContainer() { 
+
+    //     if(IS_WHEELS_ENABLED) 
+
+    //     configureBindings(); 
+
+    // } 
+
+ 
+
+    // RobotContainer constructor with gamepiece handling motors passed in from Robot.java. 
+
+    public RobotContainer( 
+
+        // Gamepiece handling motors 
+
+        TalonFX motorFeed, TalonFX motorColumn, TalonFX motorIntakeCollect, TalonFX motorIntakePivot, TalonFX motorShooterLeft, TalonFX motorShooterRight 
+
+    ) { 
+
+        // Initialize the motors for gamepiece handling 
+
+        MotorFeed = motorFeed; 
+
+        MotorColumn = motorColumn; 
+
+        MotorIntakeCollect = motorIntakeCollect; 
+
+        MotorIntakePivot = motorIntakePivot; 
+
+        MotorShooterLeft = motorShooterLeft; 
+
+        MotorShooterRight = motorShooterRight; 
+
+ 
+
+        if(IS_WHEELS_ENABLED) 
+
+            configureBindings(); 
+
+    } 
+
+ 
+
+    // Gamepiece handling motors passed from Robot.java 
+
+    private final TalonFX MotorFeed, MotorColumn, MotorIntakeCollect, MotorIntakePivot, MotorShooterLeft, MotorShooterRight; 
+
+ 
+
+    private void configureBindings() { 
+
+        // Note that X is defined as forward according to WPILib convention, 
+
+        // and Y is defined as to the left according to WPILib convention. 
+
+        drivetrain.setDefaultCommand( 
+
+            // Drivetrain will execute this command periodically 
+
+            drivetrain.applyRequest(() -> 
+
+                drive.withVelocityX(-joystick.getLeftY() * MaxSpeed) // Drive forward with negative Y (forward) 
+
+                    .withVelocityY(-joystick.getLeftX() * MaxSpeed) // Drive left with negative X (left) 
+
+                    .withRotationalRate(-joystick.getRightX() * MaxAngularRate) // Drive counterclockwise with negative X (left) 
+
+            ) 
+
+        ); 
+
+        // Idle while the robot is disabled. This ensures the configured 
+        // neutral mode is applied to the drive motors while disabled. 
+        final var idle = new SwerveRequest.Idle(); 
+        RobotModeTriggers.disabled().whileTrue( 
+            drivetrain.applyRequest(() -> idle).ignoringDisable(true) 
+        ); 
+
+        joystick.a().whileTrue(drivetrain.applyRequest(() -> brake)); 
+        joystick.b().whileTrue(drivetrain.applyRequest(() -> 
+            point.withModuleDirection(new Rotation2d(-joystick.getLeftY(), -joystick.getLeftX())) 
+        )); 
+
+        // Run SysId routines when holding back/start and X/Y. 
+        // Note that each routine should be run exactly once in a single log. 
+        joystick.back().and(joystick.y()).whileTrue(drivetrain.sysIdDynamic(Direction.kForward)); 
+        joystick.back().and(joystick.x()).whileTrue(drivetrain.sysIdDynamic(Direction.kReverse)); 
+        joystick.start().and(joystick.y()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kForward)); 
+        joystick.start().and(joystick.x()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse)); 
+
+        // Reset the field-centric heading on left bumper press. 
+        joystick.start().onTrue(drivetrain.runOnce(drivetrain::seedFieldCentric)); 
+
+        drivetrain.registerTelemetry(logger::telemeterize); 
+    } 
+
+    public Command getAutonomousCommand() { 
+
+        // Initial trajectory code. Commented out until a command can be built from it. 
+        // TrajectoryConfig config = new TrajectoryConfig(2, 1) 
+        //         .setKinematics(drivetrain.getKinematics()); 
+
+        // Trajectory trajectory = TrajectoryGenerator.generateTrajectory( 
+        //         List.of(new Pose2d(0, 0, new Rotation2d(0)), new Pose2d(-1, 0, new Rotation2d(0))), 
+        //         config); 
+
+        // Drive backwards auto. 
+        final var idle = new SwerveRequest.Idle(); 
+
+        Command backUpCommand = Commands.sequence( 
+                drivetrain.runOnce(() -> drivetrain.seedFieldCentric(Rotation2d.kZero)), // Reseting your field orientation 
+
+                drivetrain.applyRequest(() -> drive.withVelocityX(-0.5).withVelocityY(0).withRotationalRate(0)) // Create your drive command 
+                        .withTimeout(3.0), // End my drive command after 3 seconds 
+                drivetrain.applyRequest(() -> drive.withVelocityX(0).withVelocityY(0).withRotationalRate(0)), // Stop my drivetrain from moving after the previous command ends.
+
+                new RunCommand(() -> { 
+                    MotorShooterLeft.set(-0.85); 
+                    MotorShooterRight.set(-0.85); 
+                }).withTimeout(1.0), // Run the shooter for 1 second initially and do not stop 
+
+                new RunCommand(() -> { 
+                    MotorFeed.set(0.5); 
+                    MotorColumn.set(-0.5); 
+                }).withTimeout(16), // Run the Feed/Column for 16 seconds / rest of auto 
+
+                new InstantCommand(() -> { 
+                    MotorFeed.set(0); 
+                    MotorColumn.set(0); 
+                    MotorShooterLeft.set(0); 
+                    MotorShooterRight.set(0); 
+                })  // Stops all gamepiece handling motors 
+        ); 
+        return backUpCommand; 
+    } 
+} 
+
+ 
